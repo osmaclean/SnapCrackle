a87d6e4270ce69a847665625d97ec117e6f70613
Lucas Maclean <contatolucasmaclean@gmail.com>
2022-11-14 17:32:08 -0300

    Criado dois jeitos para se realizar a mesma função

---
+++ script/script.js
@@ -0,0 +1,41 @@
+// SnapCrackle Tarefa 
+
+function snapCrackle(maxValue) {
+    let texto = ""
+    for (let i = 1; i <= maxValue; i++) {
+        if ((i  % 5 === 0) && (i % 2 == 1)) {
+            texto += "SnapCrackle, "
+        } else if (i  % 5 === 0) {
+            texto += "Crackle, "
+        } else if (i % 2 == 1) {
+            texto += "Snap, "
+        } else {
+            texto += (i + ",")
+           }
+    }
+    return texto
+}
+
+////////////////////////////////////////
+
+// Segundo Modo a ser feito
+
+// function snapCrackle(maxValue) {
+//     let texto = ""
+//     let inn
+//     for (inn = 1; inn <= maxValue; inn++) {
+//         if( (inn % 5 === 0) && (inn % 2 !== 0)) {
+//             texto += "SnapCrackle, "
+//         } else if (inn  % === 0) {    
+//                  texto += "Crackle, "
+//              } else if (inn % 2 !== 0) {
+//                      texto += "Snap, "
+//                  } else {
+//                        texto += (inn + ",")
+//                      }
+        
+//     }
+//     return texto
+// }
+// console.log(snapCrackle(100));
+
